f12bdc03900d436c9f64e29a0b5ad30d23349a66
radcli14 <radcli14@gmail.com>
2022-05-08 14:18:55 -0400

    new project

---
+++ SwiftyTwoLinks/SwiftyTwoLinks/SwiftyTwoLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyTwoLinksApp.swift
+//  SwiftyTwoLinks
+//
+//  Created by Eliott Radcliffe on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyTwoLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
