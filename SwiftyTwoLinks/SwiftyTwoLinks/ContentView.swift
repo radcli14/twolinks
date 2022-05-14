333d63dbfc678dd9960537c61839f48f782dc42c
radcli14 <radcli14@gmail.com>
2022-05-14 09:08:07 -0400

    moved controls into a sheet

--- SwiftyTwoLinks/SwiftyTwoLinks/ContentView.swift
+++ SwiftyTwoLinks/SwiftyTwoLinks/ContentView.swift
@@ -18,6 +18,9 @@ struct ContentView: View {
     @State private var dimensionSlidersVisible: Bool = false
     @State private var linkOneLengthSliderVal: Double = 0.5
     @State private var linkOneOffsetSliderVal: Double = 0.5
+    @State private var pivotSliderVal: Double = 0.5
+    @State private var linkTwoLengthSliderVal: Double = 0.5
+    @State private var linkTwoOffsetSliderVal: Double = 0.5
     
     var body: some View {
         ZStack {
@@ -61,38 +64,82 @@ struct ContentView: View {
                     
                 }
 
-                if (dimensionSlidersVisible) {
+                /*if (dimensionSlidersVisible) {
                     VStack {
+                        //Spacer()
+                        
                         Text("Link One Length")
                             .font(.caption)
                             .foregroundColor(Color.white)
-                            .frame(height: 6.0)
+                            //.frame(height: 6.0)
                         Slider(value: $linkOneLengthSliderVal, in: 1...100)
-                            .frame(height: 6.0)
+                            //.frame(height: 6.0)
                             .onChange(of: linkOneLengthSliderVal) {newValue in
                                 viewController.twoLinks.setLinkOneLengthFromNorm(
                                     value: linkOneLengthSliderVal
                                 )
                                 updateSliders()
                             }
+                        
                         Text("Link One Offset")
                             .font(.caption)
                             .foregroundColor(Color.white)
-                            .frame(height: 6.0)
+                            //.frame(height: 6.0)
                         Slider(value: $linkOneOffsetSliderVal, in: 1...100)
-                            .frame(height: 6.0)
+                            //.frame(height: 6.0)
                             .onChange(of: linkOneOffsetSliderVal) {newValue in
                                 viewController.twoLinks.setLinkOneOffsetFromNorm(
                                     value: linkOneOffsetSliderVal
                                 )
                                 updateSliders()
                             }
-                        Spacer()
+                        
+                        Text("Pivot")
+                            .font(.caption)
+                            .foregroundColor(Color.white)
+                            //.frame(height: 6.0)
+                        Slider(value: $pivotSliderVal, in: 1...100)
+                            //.frame(height: 6.0)
+                            .onChange(of: pivotSliderVal) {newValue in
+                                viewController.twoLinks.setPivotFromNorm(
+                                    value: pivotSliderVal
+                                )
+                                updateSliders()
+                            }
+                        
+                        Text("Link Two Length")
+                            .font(.caption)
+                            .foregroundColor(Color.white)
+                            //.frame(height: 6.0)
+                        Slider(value: $linkTwoLengthSliderVal, in: 1...100)
+                            //.frame(height: 6.0)
+                            .onChange(of: linkTwoLengthSliderVal) {newValue in
+                                viewController.twoLinks.setLinkTwoLengthFromNorm(
+                                    value: linkTwoLengthSliderVal
+                                )
+                                updateSliders()
+                            }
+                        
+                        Text("Link Two Offset")
+                            .font(.caption)
+                            .foregroundColor(Color.white)
+                            //.frame(height: 6.0)
+                        Slider(value: $linkTwoOffsetSliderVal, in: 1...100)
+                            //.frame(height: 6.0)
+                            .onChange(of: linkTwoOffsetSliderVal) {newValue in
+                                viewController.twoLinks.setLinkTwoOffsetFromNorm(
+                                    value: linkTwoOffsetSliderVal
+                                )
+                                updateSliders()
+                            }
+                        
+                        //Spacer()
                     }
-                    .padding(12)
-                } else {
+                    //.padding(12)
+                    */
+                //} else {
                     Spacer()
-                }
+                //}
                 
                 // Hold the controls on the right side for link dimensions
                 VStack {
@@ -115,12 +162,87 @@ struct ContentView: View {
 
             }
             .padding(stackPadding)
+            .sheet(isPresented: $dimensionSlidersVisible) {
+                VStack {
+                    //Spacer()
+                    
+                    Text("Link One Length")
+                        .font(.caption)
+                        //.foregroundColor(Color.white)
+                        //.frame(height: 6.0)
+                    Slider(value: $linkOneLengthSliderVal, in: 1...100)
+                        //.frame(height: 6.0)
+                        .onChange(of: linkOneLengthSliderVal) {newValue in
+                            viewController.twoLinks.setLinkOneLengthFromNorm(
+                                value: linkOneLengthSliderVal
+                            )
+                            updateSliders()
+                        }
+                    
+                    Text("Link One Offset")
+                        .font(.caption)
+                        //.foregroundColor(Color.white)
+                        //.frame(height: 6.0)
+                    Slider(value: $linkOneOffsetSliderVal, in: 1...100)
+                        //.frame(height: 6.0)
+                        .onChange(of: linkOneOffsetSliderVal) {newValue in
+                            viewController.twoLinks.setLinkOneOffsetFromNorm(
+                                value: linkOneOffsetSliderVal
+                            )
+                            updateSliders()
+                        }
+                    
+                    Text("Pivot")
+                        .font(.caption)
+                        //.foregroundColor(Color.white)
+                        //.frame(height: 6.0)
+                    Slider(value: $pivotSliderVal, in: 1...100)
+                        //.frame(height: 6.0)
+                        .onChange(of: pivotSliderVal) {newValue in
+                            viewController.twoLinks.setPivotFromNorm(
+                                value: pivotSliderVal
+                            )
+                            updateSliders()
+                        }
+                    
+                    Text("Link Two Length")
+                        .font(.caption)
+                        //.foregroundColor(Color.white)
+                        //.frame(height: 6.0)
+                    Slider(value: $linkTwoLengthSliderVal, in: 1...100)
+                        //.frame(height: 6.0)
+                        .onChange(of: linkTwoLengthSliderVal) {newValue in
+                            viewController.twoLinks.setLinkTwoLengthFromNorm(
+                                value: linkTwoLengthSliderVal
+                            )
+                            updateSliders()
+                        }
+                    
+                    Text("Link Two Offset")
+                        .font(.caption)
+                        //.foregroundColor(Color.white)
+                        //.frame(height: 6.0)
+                    Slider(value: $linkTwoOffsetSliderVal, in: 1...100)
+                        //.frame(height: 6.0)
+                        .onChange(of: linkTwoOffsetSliderVal) {newValue in
+                            viewController.twoLinks.setLinkTwoOffsetFromNorm(
+                                value: linkTwoOffsetSliderVal
+                            )
+                            updateSliders()
+                        }
+                    
+                    //Spacer()
+                }
+            }
         }
     }
     
     func updateSliders() {
         linkOneLengthSliderVal = viewController.twoLinks.linkOneLengthNorm
         linkOneOffsetSliderVal = viewController.twoLinks.linkOneOffsetNorm
+        pivotSliderVal = viewController.twoLinks.pivotNorm
+        linkTwoLengthSliderVal = viewController.twoLinks.linkTwoLengthNorm
+        linkTwoOffsetSliderVal = viewController.twoLinks.linkTwoOffsetNorm
     }
 }
 
