e54e77e80e5c864d74b701c2c3c0e8d1de06ef82
radcli14 <radcli14@gmail.com>
2022-06-05 21:53:13 -0400

    formatted the buttons and icons

--- KotlyOtlyDobleDosLinks/app/src/main/java/dcsimulationstudio/kotlyotlydobledoslinks/MainActivity.kt
+++ KotlyOtlyDobleDosLinks/app/src/main/java/dcsimulationstudio/kotlyotlydobledoslinks/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Load the buttons that the user uses to control playback, or modify configuration
+        val restartButton = findViewById(R.id.restart_button)
+        val playButton = findViewById(R.id.play_button)
+        val editButton = findViewById(R.id.edit_button)
+        val colorButton = findViewById(R.id.color_button)
     }
 }
