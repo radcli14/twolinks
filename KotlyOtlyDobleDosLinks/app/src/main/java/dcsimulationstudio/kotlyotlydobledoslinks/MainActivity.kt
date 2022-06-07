aaedc4c67589f3f709c3a550686027120554813e
radcli14 <radcli14@gmail.com>
2022-06-06 21:47:56 -0400

    added icons and assets

--- KotlyOtlyDobleDosLinks/app/src/main/java/dcsimulationstudio/kotlyotlydobledoslinks/MainActivity.kt
+++ KotlyOtlyDobleDosLinks/app/src/main/java/dcsimulationstudio/kotlyotlydobledoslinks/MainActivity.kt
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.graphics.scaleMatrix
 import androidx.lifecycle.lifecycleScope
 import com.google.android.filament.utils.HDRLoader
+import com.google.android.filament.utils.scale
 import io.github.sceneview.SceneView
 import io.github.sceneview.environment.loadEnvironment
 import io.github.sceneview.math.Position
 import io.github.sceneview.math.Rotation
+import io.github.sceneview.math.Scale
 import io.github.sceneview.node.ModelNode
 import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sceneView: SceneView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,41 +29,51 @@ class MainActivity : AppCompatActivity() {
         val editButton = findViewById<Button>(R.id.edit_button)
         val colorButton = findViewById<Button>(R.id.color_button)
 
+        // Set the listeners
+        restartButton.setOnClickListener { println("restart!!!") }
+        playButton.setOnClickListener { println("play!!!") }
+        editButton.setOnClickListener { println("edit!!!") }
+        colorButton.setOnClickListener { println("color???") }
+
         // Load the SceneView, used for 3D rendering
-        val sceneView = findViewById<SceneView>(R.id.sceneView)
+        sceneView = findViewById<SceneView>(R.id.sceneView)
 
         // Demo from github below this line
-        sceneView.camera.position = Position(x = 4.0f, y = -1.0f)
-        sceneView.camera.rotation = Rotation(x = 0.0f, y = 80.0f)
+        sceneView.camera.position = Position(x = 0.0f, y = 0.0f, z = 2.5f)
+        //sceneView.camera.rotation = Rotation(z = 180f)
 
-        val modelNode = ModelNode()
-        sceneView.addChild(modelNode)
+        val moonNode = ModelNode()
+        sceneView.addChild(moonNode)
+
+        val doorNode = ModelNode()
+        sceneView.addChild(doorNode)
 
         lifecycleScope.launchWhenCreated {
             sceneView.environment = HDRLoader.loadEnvironment(
                 context = this@MainActivity,
                 lifecycle = lifecycle,
                 hdrFileLocation = "environments/studio_small_09_2k.hdr",
-                specularFilter = false
+                //specularFilter = false,
+                createSkybox = false
             )
 
-            modelNode.loadModel(
+            moonNode.loadModel(
                 context = this@MainActivity,
                 lifecycle = lifecycle,
-                glbFileLocation = "https://sceneview.github.io/assets/models/MaterialSuite.glb",
-                autoAnimate = true,
-                autoScale = true,
-                centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
+                glbFileLocation = "models/point.glb",
+                //centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
             )
-            // We currently have an issue while the model render is not completely loaded
-            delay(200)
-            //isLoading = false
-            sceneView.camera.smooth(
-                position = Position(x = -1.0f, y = 1.5f, z = -3.5f),
-                rotation = Rotation(x = -60.0f, y = -50.0f),
-                speed = 0.5f
+            moonNode.position = Position(0.0f, -32.415f, 0.0f)
+            moonNode.scale = Scale(31.4f, 31.4f, 31.4f)
+
+            doorNode.loadModel(
+                context = this@MainActivity,
+                lifecycle = lifecycle,
+                glbFileLocation = "models/box.glb",
+                //centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
             )
+            //doorNode.position = Position(0.0f, 0.0f, 0.0f)
+            doorNode.scale = Scale(0.91f, 2.03f, 0.035f)
         }
     }
-
 }
