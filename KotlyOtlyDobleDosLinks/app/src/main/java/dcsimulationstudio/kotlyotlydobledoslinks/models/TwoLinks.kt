ee4caf987a32ebe863d5d70bdabc8dec732899d6
radcli14 <radcli14@gmail.com>
2022-06-08 21:04:55 -0400

    no errors in the TwoLinks class, and all necessary methods are implemented

--- KotlyOtlyDobleDosLinks/app/src/main/java/dcsimulationstudio/kotlyotlydobledoslinks/models/TwoLinks.kt
+++ KotlyOtlyDobleDosLinks/app/src/main/java/dcsimulationstudio/kotlyotlydobledoslinks/models/TwoLinks.kt
@@ -4,18 +4,46 @@ import com.google.android.filament.utils.Float3
 import dev.romainguy.kotlin.math.Float4
 import dev.romainguy.kotlin.math.pow
 import kotlin.math.cos
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.sin
 
-/*
-func rk4(equation: (Double, simd_double4) -> simd_double4, t: Double, x: simd_double4, h: Double) -> simd_double4 {
+fun rk4(equation: (Float, Float4) -> Float4, t: Float, x: Float4, h: Float): Float4 {
     // Apply the 4th order Runge-Kutta integration routine
-    let k1 = equation(t, x)
-    let k2 = equation(t, x + 0.5 * h * k1)
-    let k3 = equation(t, x + 0.5 * h * k2)
-    let k4 = equation(t, x + h * k3)
-    return x + 1.0/6.0 * h * (k1 + 2.0*k2 + 2.0*k3 + k4)
+    val k1 = equation(t, x)
+    val k2 = equation(t, rk4xPlusHK(x, 0.5f*h, k1))  // x + 0.5 * h * k1)
+    val k3 = equation(t, rk4xPlusHK(x, 0.5f*h, k2))  // x + 0.5 * h * k2)
+    val k4 = equation(t, rk4xPlusHK(x, h, k3))  // x + h * k3)
+    //return x + 1.0/6.0 * h * (k1 + 2.0*k2 + 2.0*k3 + k4)
+    val oneSixth = 1.0f / 6.0f
+    return Float4(
+        x[0] + oneSixth * h * (k1[0] + 2.0f*k2[0] + 2.0f*k3[0] + k4[0]),
+        x[1] + oneSixth * h * (k1[1] + 2.0f*k2[1] + 2.0f*k3[1] + k4[1]),
+        x[2] + oneSixth * h * (k1[2] + 2.0f*k2[2] + 2.0f*k3[2] + k4[2]),
+        x[3] + oneSixth * h * (k1[3] + 2.0f*k2[3] + 2.0f*k3[3] + k4[3]),
+    )
 }
- */
+
+fun rk4xPlusHK(x: Float4, h: Float, k: Float4): Float4 {
+    return Float4(
+        x[0] + h * k[0],
+        x[1] + h * k[1],
+        x[2] + h * k[2],
+        x[3] + h * k[3]
+    )
+}
+
+fun invert2x2(matrix: Array<FloatArray>): Array<FloatArray> {
+    val a = matrix[0][0]
+    val b = matrix[0][1]
+    val c = matrix[1][0]
+    val d = matrix[1][1]
+    val det = 1.0f / (a*d - b*c)
+    return arrayOf(
+        floatArrayOf(d * det, -b * det),
+        floatArrayOf(-c * det, a * det)
+    )
+}
 
 class TwoLinks {
     var length = floatArrayOf(0.28f, 0.23f)
@@ -26,7 +54,7 @@ class TwoLinks {
     var thickness = floatArrayOf(0.0064f, 0.0064f)
 
     var density = floatArrayOf(800.0f, 800.0f)
-    var dampingRatio = 0.0f
+    //var dampingRatio = 0.0f
     var θ = floatArrayOf(0.0f, 0.0f)
     var ω = floatArrayOf(0.0f, 0.0f)
 
@@ -45,8 +73,7 @@ class TwoLinks {
     private var _m22: Float? = null
 
     val position: Array<Float3>
-        get() {
-            return arrayOf(
+        get() = arrayOf(
                 Float3(
                     offset[0] * cos(θ[0]),
                     offset[0] * sin(θ[0]),
@@ -58,50 +85,37 @@ class TwoLinks {
                     thickness[0] + thickness[1]
                 )
             )
-        }
 
     val pivotPosition: Float3
-        get() {
-            return Float3(
+        get() = Float3(
                 pivot * cos(θ[0]),
                 pivot * sin(θ[0]),
                 thickness[0] + 0.5f * thickness[1]
             )
-        }
 
     val orientation: Array<Float4>
-        get() {
-            return arrayOf(
+        get() = arrayOf(
                 Float4(0f, 0f, sin(0.5f * θ[0]), cos(0.5f * θ[0])),
                 Float4(0f, 0f, sin(0.5f * θ[1]), cos(0.5f * θ[1]))
             )
-        }
 
     val mass: FloatArray
-        get() {
-            return _mass ?: floatArrayOf(
+        get() = _mass ?: floatArrayOf(
                 density[0] * length[0] * height[0] * thickness[0],
                 density[1] * length[1] * height[1] * thickness[1]
             )
-        }
 
     val moi: FloatArray
-        get() {
-            return _moi ?: floatArrayOf(
+        get() = _moi ?: floatArrayOf(
                 1.0f/12.0f * mass[0] * (pow(length[0], 2f) + pow(height[0], 2f)),
                 1.0f/12.0f * mass[1] * (pow(length[1], 2f) + pow(height[1], 2f))
             )
-        }
 
     val m11: Float
-        get() {
-            return _m11 ?: (moi[0] + mass[0] * pow(offset[0], 2f) + mass[1] * pow(pivot, 2f))
-        }
+        get() = _m11 ?: (moi[0] + mass[0] * pow(offset[0], 2f) + mass[1] * pow(pivot, 2f))
 
     val m22: Float
-        get() {
-            return _m22 ?: (moi[1] + mass[1] * pow(offset[1], 2f))
-        }
+        get() = _m22 ?: (moi[1] + mass[1] * pow(offset[1], 2f))
 
     fun m12(x: Float4): Float {
         return mass[1] * pivot * offset[1] * cos(x[0] - x[1])
@@ -115,135 +129,122 @@ class TwoLinks {
         )
     }
 
-    /*
-    func forcing(x: simd_double4) -> simd_double2 {
-        let g_x = grav * (manager.accelerometerData?.acceleration.x ?? 0.0)
-        let g_y = grav * (manager.accelerometerData?.acceleration.y ?? -1.0)
-        let a = pivot
-        let b = offset[0]
-        let c = offset[1]
-        let m_0 = mass[0]
-        let m_1 = mass[1]
-        let θ0 = x[0]
-        let θ1 = x[1]
-        let ω0 = x[2]
-        let ω1 = x[3]
-        return simd_double2(
-            -a*c*m_1*pow(ω1, 2)*sin(θ0 - θ1) - a*g_x*m_1*sin(θ0) + a*g_y*m_1*cos(θ0) - b*g_x*m_0*sin(θ0) + b*g_y*m_0*cos(θ0),
-             c*m_1*(a*pow(ω0, 2)*sin(θ0 - θ1) - g_x*sin(θ1) + g_y*cos(θ1))
+    fun forcing(x: Float4): FloatArray {
+        val gx = 0.0f  // grav * (manager.accelerometerData?.acceleration.x ?? 0.0)
+        val gy = -grav * 1.0f  // grav * (manager.accelerometerData?.acceleration.y ?? -1.0)
+        val a = pivot
+        val b = offset[0]
+        val c = offset[1]
+        val m0 = mass[0]
+        val m1 = mass[1]
+        val θ0 = x[0]
+        val θ1 = x[1]
+        val ω0 = x[2]
+        val ω1 = x[3]
+        return floatArrayOf(
+            -a*c*m1*pow(ω1, 2f)*sin(θ0 - θ1) - a*gx*m1*sin(θ0) + a*gy*m1*cos(θ0) - b*gx*m0*sin(θ0) + b*gy*m0*cos(θ0),
+             c*m1*(a*pow(ω0, 2f)*sin(θ0 - θ1) - gx*sin(θ1) + gy*cos(θ1))
         )
     }
 
-    func equationOfMotion(t: Double, x: simd_double4) -> simd_double4 {
+    fun equationOfMotion(t: Float, x: Float4): Float4 {
         // Calculate the angular velocity and acceleration states given angle and angular velocity
-        let invM = massMatrix(x: x).inverse
-        let F = forcing(x: x)
-        let dx = simd_mul(invM, F)
-        return simd_double4(x[2], x[3], dx[0], dx[1])
+        val invM = invert2x2(massMatrix(x))
+        val f = forcing(x)
+        val dx = floatArrayOf(
+            invM[0][0]*f[0] + invM[0][1]*f[1],
+            invM[1][0]*f[0] + invM[1][1]*f[1]
+        )
+        return Float4(x[2], x[3], dx[0], dx[1])
     }
 
-    func update() {
+    fun update() {
         // Calculate states at the next frame
-        let priorState = simd_double4(θ[0], θ[1], ω[0], ω[1])
-        let newState = rk4(equation: equationOfMotion, t: 0.0, x: priorState, h: dt)
-        θ = [newState[0], newState[1]]
-        ω = [newState[2], newState[3]]
+        val priorState = Float4(θ[0], θ[1], ω[0], ω[1])
+        val newState = rk4({t, x -> equationOfMotion(t, x)}, 0.0f, priorState, dt)
+        θ = floatArrayOf(newState[0], newState[1])
+        ω = floatArrayOf(newState[2], newState[3])
     }
 
-    func nilify() {
+    fun nullify() {
         // When a specified value changes, this makes sure the
         // properties below get re-calculated on next update
-        _mass = nil
-        _moi = nil
-        _m11 = nil
-        _m22 = nil
+        _mass = null
+        _moi = null
+        _m11 = null
+        _m22 = null
     }
 
-    var linkOneLengthNorm: Double {
-        get {
-            return (length[0] - minLength) / (maxLength - minLength)
-        }
-    }
+    val linkOneLengthNorm: Float
+        get() = (length[0] - minLength) / (maxLength - minLength)
 
-    func setLinkOneLengthFromNorm(value: Double) {
+    fun setLinkOneLengthFromNorm(value: Float) {
         // Get the norm values before adjusting, to avoid recursion
-        let offsetNorm = linkOneOffsetNorm
+        val offsetNorm = linkOneOffsetNorm
 
         // Adjust the physical distances
         length[0] = minLength + value * (maxLength - minLength)
-        _linkOneGeometry.width = length[0]
-        offset[0] = (1.0 - offsetNorm) * (0.5 * length[0] - minDistanceFromEdge)
+        //_linkOneGeometry.width = length[0]
+        offset[0] = (1.0f - offsetNorm) * (0.5f * length[0] - minDistanceFromEdge)
         pivot = min(pivot, maxPivot)
 
         // Call this to make sure the mass properties get re-calculated
-        nilify()
+        nullify()
     }
 
-    var linkOneOffsetNorm: Double {
-        get {
-            let n = 1.0 - offset[0] / (0.5 * length[0] - minDistanceFromEdge)
-            return min(1.0, max(0.0, n))
+    val linkOneOffsetNorm: Float
+        get() {
+            val n = 1.0f - offset[0] / (0.5f * length[0] - minDistanceFromEdge)
+            return min(1.0f, max(0.0f, n))
         }
-    }
 
-    func setLinkOneOffsetFromNorm(n: Double) {
-        offset[0] = (1.0 - n) * (0.5 * length[0] - minDistanceFromEdge)
+    fun setLinkOneOffsetFromNorm(n: Float) {
+        offset[0] = (1.0f - n) * (0.5f * length[0] - minDistanceFromEdge)
         pivot = min(pivot, maxPivot)
 
         // Call this to make sure the mass properties get re-calculated
-        nilify()
+        nullify()
     }
 
-    var maxPivot: Double {
-        get {
-            return 0.5 * length[0] - minDistanceFromEdge + offset[0]
-        }
-    }
+    val maxPivot: Float
+        get() = 0.5f * length[0] - minDistanceFromEdge + offset[0]
 
-    var pivotNorm: Double {
-        get {
-            return min(1.0, max(0.0, pivot / maxPivot))
-        }
-    }
+    val pivotNorm: Float
+        get() = min(1.0f, max(0.0f, pivot / maxPivot))
 
-    func setPivotFromNorm(m: Double) {
+    fun setPivotFromNorm(m: Float) {
         pivot = m * maxPivot
 
         // Call this to make sure the mass properties get re-calculated
-        nilify()
+        nullify()
     }
 
-    var linkTwoLengthNorm: Double {
-        get {
-            return (length[1] - minLength) / (maxLength - minLength)
-        }
-    }
+    val linkTwoLengthNorm: Float
+        get() = (length[1] - minLength) / (maxLength - minLength)
 
-    func setLinkTwoLengthFromNorm(n: Double) {
+    fun setLinkTwoLengthFromNorm(n: Float) {
         // Get the norm values before adjusting, to avoid recursion
-        let offsetNorm = linkTwoOffsetNorm
+        val offsetNorm = linkTwoOffsetNorm
 
         // Adjust the physical distances
         length[1] = minLength + n * (maxLength - minLength)
-        _linkTwoGeometry.width = length[1]
-        offset[1] = (1.0 - offsetNorm) * (0.5 * length[1] - minDistanceFromEdge)
+        //_linkTwoGeometry.width = length[1]
+        offset[1] = (1.0f - offsetNorm) * (0.5f * length[1] - minDistanceFromEdge)
 
         // Call this to make sure the mass properties get re-calculated
-        nilify()
+        nullify()
     }
 
-    var linkTwoOffsetNorm: Double {
-        get {
-            let n = 1.0 - offset[1] / (0.5 * length[1] - minDistanceFromEdge)
-            return min(1.0, max(0.0, n))
+    val linkTwoOffsetNorm: Float
+        get() {
+            val n = 1.0f - offset[1] / (0.5f * length[1] - minDistanceFromEdge)
+            return min(1.0f, max(0.0f, n))
         }
-    }
 
-    func setLinkTwoOffsetFromNorm(n: Double) {
-        offset[1] = (1.0 - n) * (0.5 * length[1] - minDistanceFromEdge)
+    fun setLinkTwoOffsetFromNorm(n: Float) {
+        offset[1] = (1.0f - n) * (0.5f * length[1] - minDistanceFromEdge)
 
         // Call this to make sure the mass properties get re-calculated
-        nilify()
+        nullify()
     }
-     */
 }
