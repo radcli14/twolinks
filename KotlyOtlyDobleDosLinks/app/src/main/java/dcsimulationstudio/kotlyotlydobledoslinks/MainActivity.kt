9b71cc8cf8b3c73dfdafbe39535a93194f07edc6
radcli14 <radcli14@gmail.com>
2022-06-11 15:43:39 -0400

    organized, created functions to generate planets, boxes, and hinges

--- KotlyOtlyDobleDosLinks/app/src/main/java/dcsimulationstudio/kotlyotlydobledoslinks/MainActivity.kt
+++ KotlyOtlyDobleDosLinks/app/src/main/java/dcsimulationstudio/kotlyotlydobledoslinks/MainActivity.kt
@@ -14,16 +14,19 @@ import io.github.sceneview.environment.loadEnvironment
 import io.github.sceneview.material.setBaseColor
 import io.github.sceneview.material.setMetallicFactor
 import io.github.sceneview.math.Position
+import io.github.sceneview.math.Rotation
 import io.github.sceneview.math.Scale
 import io.github.sceneview.node.ModelNode
+import kotlin.random.Random
 
 const val TAG = "Main"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var sceneView: SceneView
     private lateinit var choreographer: Choreographer
-    val linkOneNode = ModelNode()
-    val linkTwoNode = ModelNode()
+    private lateinit var linkOne: ModelNode
+    private lateinit var linkTwo: ModelNode
+    private lateinit var pivot: ModelNode
 
     // Get the view model
     val viewModel: MainViewModel by viewModels()
@@ -59,20 +62,13 @@ class MainActivity : AppCompatActivity() {
         // Load the SceneView, used for 3D rendering
         sceneView = findViewById(R.id.sceneView)
 
-        // Demo from github below this line
-        sceneView.camera.position = Position(x = 0.0f, y = 0.0f, z = 2.5f)
-        //sceneView.camera.rotation = Rotation(z = 180f)
-
-        val moonNode = ModelNode()
-        sceneView.addChild(moonNode)
-
-        val doorNode = ModelNode()
-        sceneView.addChild(doorNode)
-
-        sceneView.addChild(linkOneNode)
-        sceneView.addChild(linkTwoNode)
-
+        // Build the scene
         lifecycleScope.launchWhenCreated {
+            // Create the camera, initially at a position in front of the door, orbits around it
+            sceneView.camera.position = Position(x = 0.0f, y = 0.0f, z = 2.5f)
+            //sceneView.camera.rotation = Rotation(z = 180f)
+
+            // The environment defines the image based lighting
             sceneView.environment = HDRLoader.loadEnvironment(
                 context = this@MainActivity,
                 lifecycle = lifecycle,
@@ -82,56 +78,125 @@ class MainActivity : AppCompatActivity() {
             )
 
             // Load the sphere model to form the moon part of the background
-            moonNode.loadModel(
-                context = this@MainActivity,
-                lifecycle = lifecycle,
-                glbFileLocation = "models/sphere.glb",
-                centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
+            val moon = makePlanet(
+                y = -32.415f,
+                radius = 31.4f,
+                red = 0.5f,
+                green = 0.5f,
+                blue = 0.5f
             )
-            moonNode.scale = Scale(31.4f, 31.4f, 31.4f)
-            moonNode.position = Position(0.0f, -32.415f, 0.0f)
-
-            // Set the visual properties of the moon
-            val moonMaterial = moonNode.modelInstance?.material?.filamentMaterialInstance
-            moonMaterial?.setBaseColor(Float4(0.5f, 0.5f, 0.5f, 1.0f))
+            sceneView.addChild(moon)
 
             // Load the box model to form the door part of the background
-            doorNode.loadModel(
-                context = this@MainActivity,
-                lifecycle = lifecycle,
-                glbFileLocation = "models/box.glb",
-                centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
+            val door = makeBox(
+                z = -0.0175f,
+                scaleX = 0.91f,
+                scaleY = 2.03f,
+                scaleZ = 0.035f,
+                metallic = 1f
             )
-            doorNode.position = Position(0.0f, 0.0f, -0.0175f)  //-0.0175f)
-            doorNode.scale = Scale(0.91f, 2.03f, 0.035f)
+            sceneView.addChild(door)
 
-            // Set the visual properties of the door
-            val doorMat = doorNode.modelInstance?.material?.filamentMaterialInstance
-            doorMat?.setBaseColor(Float4(0.0f, 0.0f, 0.0f, 1.0f))
-            doorMat?.setMetallicFactor(1.0f)
-
-            // Define the first pendulum link
-            linkOneNode.loadModel(
-                context = this@MainActivity,
-                lifecycle = lifecycle,
-                glbFileLocation = "models/box.glb",
-                centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
+            // Load the hinges at the origin and pivot point
+            val hinge = makeHinge(
+                z = viewModel.twoLinks.thickness[0]
             )
-            linkOneNode.scale = Scale(viewModel.twoLinks.length[0], viewModel.twoLinks.height[0], viewModel.twoLinks.thickness[0])
-            val linkOneMaterial = linkOneNode.modelInstance?.material?.filamentMaterialInstance
-            linkOneMaterial?.setBaseColor(Float4(1.0f, 0.0f, 0.0f, 1.0f))
-
-            // Define the second pendulum link
-            linkTwoNode.loadModel(
-                context = this@MainActivity,
-                lifecycle = lifecycle,
-                glbFileLocation = "models/box.glb",
-                centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
+            val pivotPosition = viewModel.twoLinks.pivotPosition
+            pivot = makeHinge(
+                length = 0.013f,
+                x = pivotPosition.x,
+                y = pivotPosition.y,
+                z = pivotPosition.z
             )
-            linkTwoNode.scale = Scale(viewModel.twoLinks.length[1], viewModel.twoLinks.height[1], viewModel.twoLinks.thickness[1])
-            val linkTwoMaterial = linkTwoNode.modelInstance?.material?.filamentMaterialInstance
-            linkTwoMaterial?.setBaseColor(Float4(0.0f, 1.0f, 0.0f, 1.0f))
+            sceneView.addChild(hinge)
+            sceneView.addChild(pivot)
+
+            // Define the pendulum links
+            linkOne = makeBox(
+                scaleX = viewModel.twoLinks.length[0],
+                scaleY = viewModel.twoLinks.height[0],
+                scaleZ = viewModel.twoLinks.thickness[0],
+                randomColor = true
+            )
+            linkTwo = makeBox(
+                scaleX = viewModel.twoLinks.length[1],
+                scaleY = viewModel.twoLinks.height[1],
+                scaleZ = viewModel.twoLinks.thickness[1],
+                randomColor = true
+            )
+            sceneView.addChild(linkOne)
+            sceneView.addChild(linkTwo)
+
         }
+    }
+
+    /**
+     * Load a sphere representing a planet
+     */
+    private suspend fun makePlanet(x: Float = 0f, y: Float = 0f, z: Float = 0f,
+                                   radius: Float = 1f,
+                                   red: Float = 0f, green: Float = 0f, blue: Float = 0f
+    ) : ModelNode {
+        val node = ModelNode()
+        node.loadModel(
+            context = this@MainActivity,
+            lifecycle = lifecycle,
+            glbFileLocation = "models/sphere.glb",
+            centerOrigin = Position(x = 0f, y = 0f, z = 0f)
+        )
+        node.scale = Scale(radius, radius, radius)
+        node.position = Position(x, y, z)
+        val material = node.modelInstance?.material?.filamentMaterialInstance
+        material?.setBaseColor(Float4(red, green, blue, 1.0f))
+        return node
+    }
+
+    /**
+     * Load the box model for a pre-existing node
+     */
+    private suspend fun makeBox(x: Float = 0f, y: Float = 0f, z: Float = 0f,
+                                scaleX: Float = 1f, scaleY: Float = 1f, scaleZ: Float = 1f,
+                                red: Float = 0f, green: Float = 0f, blue: Float = 0f,
+                                randomColor: Boolean = false, metallic: Float = 0f
+    ) : ModelNode {
+        val node = ModelNode()
+        node.loadModel(
+            context = this@MainActivity,
+            lifecycle = lifecycle,
+            glbFileLocation = "models/box.glb",
+            centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
+        )
+        node.position = Position(x, y, z)
+        node.scale = Scale(scaleX, scaleY, scaleZ)
+        val material = node.modelInstance?.material?.filamentMaterialInstance
+        val color = when(randomColor) {
+            true -> Float4(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f)
+            false -> Float4(red, green, blue, 1.0f)
+        }
+        material?.setBaseColor(color)
+        material?.setMetallicFactor(metallic)
+        return node
+    }
+
+    /**
+     * Load the cylinders representing the hinge points at the origin and pivot
+     */
+    private suspend fun makeHinge(x: Float = 0f, y: Float = 0f, z: Float = 0f,
+                                  length: Float = 0.007f, radius: Float = 0.007f
+    ) : ModelNode {
+        val node = ModelNode()
+        node.loadModel(
+            context = this@MainActivity,
+            lifecycle = lifecycle,
+            glbFileLocation = "models/cylinder.glb",
+            centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
+        )
+        node.position = Position(x, y, z)
+        node.scale = Scale(length, radius, radius)
+        node.rotation = Rotation(0f, 90f, 0f)
+        val material = node.modelInstance?.material?.filamentMaterialInstance
+        material?.setBaseColor(Float4(0.2f, 0.2f, 0.2f, 1.0f))
+        return node
     }
 
     override fun onResume() {
@@ -144,8 +209,12 @@ class MainActivity : AppCompatActivity() {
         choreographer.removeFrameCallback(frameCallback)
     }
 
+    // Float value for the time at the last frame, used to determine differential time
     var lastTime = 0f
 
+    /**
+     * Call once every frame, update the positions and orientations of the links
+     */
     private val frameCallback = object : Choreographer.FrameCallback {
         override fun doFrame(currentTime: Long) {
             // Obtain the timing data, dt is differential time
@@ -155,10 +224,12 @@ class MainActivity : AppCompatActivity() {
             // Make sure the scene is ready and not paused, then do the update
             if (sceneIsInitialized && !viewModel.isPaused) {
                 viewModel.update(dt)
-                linkOneNode.position = viewModel.linkOnePosition
-                linkOneNode.rotation = viewModel.linkOneRotation
-                linkTwoNode.position = viewModel.linkTwoPosition
-                linkTwoNode.rotation = viewModel.linkTwoRotation
+                linkOne.position = viewModel.linkOnePosition
+                linkOne.rotation = viewModel.linkOneRotation
+                linkTwo.position = viewModel.linkTwoPosition
+                linkTwo.rotation = viewModel.linkTwoRotation
+                pivot.position.x = viewModel.twoLinks.pivotPosition.x
+                pivot.position.y = viewModel.twoLinks.pivotPosition.y
             }
 
             // Set up the next frame update
@@ -167,8 +238,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Check that all the lateinit variables have been initialized
+     */
     private val sceneIsInitialized: Boolean
         get() {
-            return this::sceneView.isInitialized
+            return this::sceneView.isInitialized &&
+                    this::linkOne.isInitialized &&
+                    this::linkTwo.isInitialized &&
+                    this::pivot.isInitialized
         }
 }
