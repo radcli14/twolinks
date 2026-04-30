776fec0370842d8acce9fdf3961af66c2afa8fee
radcli14 <radcli14@gmail.com>
2026-04-30 13:31:38 +0200

    instantiate the view model inside the activity

--- TwoLinksCMP/composeApp/src/androidMain/kotlin/com/dcengineer/twolinks/MainActivity.kt
+++ TwoLinksCMP/composeApp/src/androidMain/kotlin/com/dcengineer/twolinks/MainActivity.kt
@@ -17,10 +17,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
-
+        
         setContent {
-            App()
+            val viewModel = MainViewModel()
+            App(viewModel)
         }
     }
 }
