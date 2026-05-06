f8fd7579a203f46f939dae9f42fee8cad91fe067
radcli14 <radcli14@gmail.com>
2026-05-06 13:43:41 +0200

    Remove explicit JsFun import

--- TwoLinksCMP/composeApp/src/wasmJsMain/kotlin/com/dcengineer/twolinks/Platform.wasmJs.kt
+++ TwoLinksCMP/composeApp/src/wasmJsMain/kotlin/com/dcengineer/twolinks/Platform.wasmJs.kt
@@ -2,7 +2,6 @@
 package com.dcengineer.twolinks
 
 import kotlin.js.ExperimentalWasmJsInterop
-import kotlin.js.JsFun
 
 @JsFun("() => { const ua = window.navigator.userAgent.toLowerCase(); return (ua.includes('iphone') || ua.includes('ipad') || ua.includes('ipod')) && ua.includes('safari') && !ua.includes('chrome') && !ua.includes('crios'); }")
 external fun isIosSafari(): Boolean
