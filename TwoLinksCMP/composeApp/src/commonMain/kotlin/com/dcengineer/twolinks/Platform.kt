a5f56c53979b62b527590e10c66d1e5f7abda360
radcli14 <radcli14@gmail.com>
2026-05-17 22:25:34 +0200

    added platform variable for whether AR is available

--- TwoLinksCMP/composeApp/src/commonMain/kotlin/com/dcengineer/twolinks/Platform.kt
+++ TwoLinksCMP/composeApp/src/commonMain/kotlin/com/dcengineer/twolinks/Platform.kt
@@ -3,6 +3,7 @@ package com.dcengineer.twolinks
 interface Platform {
     val name: String
     val hasBottomSafeArea: Boolean get() = false
+    val isARAvailable: Boolean get() = false
 }
 
 expect fun getPlatform(): Platform
--- TwoLinksCMP/composeApp/src/iosMain/kotlin/com/dcengineer/twolinks/Platform.ios.kt
+++ TwoLinksCMP/composeApp/src/iosMain/kotlin/com/dcengineer/twolinks/Platform.ios.kt
@@ -1,9 +1,11 @@
 package com.dcengineer.twolinks
 
+import platform.ARKit.ARWorldTrackingConfiguration
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    override val isARAvailable: Boolean = ARWorldTrackingConfiguration.isSupported
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
